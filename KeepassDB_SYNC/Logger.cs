d8815f01c8a8c5c07fea87f0b4392a1237db1ef3
EmileBosse <emie.bosse@uqar.ca>
2020-04-01 08:15:27 -0400

    premier commit

---
+++ KeepassDB_SYNC/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepassDB_SYNC
+{
+    public enum Type { DEBUG, WARNING, ERROR }
+
+    class Logger
+    {
+        static void Write(Type t)
+        {
+
+        }
+    }
+}
