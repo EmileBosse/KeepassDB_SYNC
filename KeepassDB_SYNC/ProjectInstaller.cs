78af71d20b0042a1b6af707dc9c3a1d2992499db
Bossé <BOSSEE@dfo-mpo.ca>
2020-04-02 06:56:19 -0400

    Project Installer - make the service installation work

---
+++ KeepassDB_SYNC/ProjectInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepassDB_SYNC
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
